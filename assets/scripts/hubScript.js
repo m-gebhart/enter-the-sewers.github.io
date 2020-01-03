9d6d1f48c3a52d4d0a84ff4c17ed3459fa508164
m-gebhart <45672199+m-gebhart@users.noreply.github.com>
2020-01-04 00:25:33 +0100

    finalized visual basics

--- assets/scripts/hubScript.js
+++ assets/scripts/hubScript.js
@@ -4,43 +4,40 @@ var keywords = [['I walk alone', 'i walk alone', 'I WALK ALONE'], ['Ghosts', 'gh
 var displayed = false;
 var inProcess = false;
 var welcomeMessagePassed = false;
-var progressionInt = -2;
+var progressionInt = -1;
 var currentEpisodeInt = 0;
-var animTimeFloat = 0.15;
+var animTimeFloat = 0.05;
+var closePopUpAnimTime = 0.5;
 var unlockedOpacity = 1.0;
 var clickedOpacity = 0.85;
-var popUpBGColor = 'rgba(0, 0, 0, 1)';
 var messageTransitionDuration = "1s";
-var textDelay = 2.0;
+var textDelay = 0.1;
 var closeVar;
+var proceedVar;
 
 window.onclick = function (event) {
     //welcomeMessage
-    if (!welcomeMessagePassed) {
-        if (progressionInt == -2) {
+    if (!welcomeMessagePassed && progressionInt == -1) 
             open_welcomeMessage(document.getElementById('welcomeMessage'));
-            progressionInt++; //to -1
-        }
-        else if (progressionInt == -1) {
-            close_welcomeMessage(document.getElementById('welcomeMessage'));
-            progressionInt++; //to 0;
-        }
-    }
 
-    //displaying first box (station)
+    //displaying arrow to first box (station)
     if (progressionInt == 0 && welcomeMessagePassed) {
         progressionInt++; //to 1 for box1
-        highlight_station(progressionInt, unlockedOpacity);
+        unlock_arrow(1);
     }
 
     //closing popUp when clicking outside of it
-    if (displayed && (event.target.classList.contains("map")
-        || event.target.classList.contains("underground_map")
-        || event.target.classList.contains("popUpBackground")))
+    if (displayed && (event.target.classList.contains("map") || event.target.classList.contains("underground_map")) || event.target.classList.contains("background"))
         close_PopUp();
 }
 
+function proceed() {
+    close_welcomeMessage(document.getElementById('welcomeMessage'));
+    progressionInt = 0;
+}
+
 function open_welcomeMessage(message) {
+    proceedVar = document.getElementById("proceed");
     load_welcomeTxtFile();
     message.style.transitionDuration = messageTransitionDuration;
     message.style.backgroundColor = "rgba(0, 0, 0, 1.0)";
@@ -59,11 +56,6 @@ function close_welcomeMessage(message) {
     })
 }
 
-function highlight_station(stationInt, opacityFloat) {
-    highlight_element(document.getElementById("box" + String(stationInt)), unlockedOpacity);
-    highlight_element(document.getElementById('arrow' + String(stationInt)), unlockedOpacity);
-}
-
 function highlight_element(element, opacityFloat) {
     element.style.transitionDuration = '1s';
     element.style.opacity = opacityFloat;
@@ -72,7 +64,6 @@ function highlight_element(element, opacityFloat) {
 function open_PopUp(episodeInt) {
     if (episodeInt <= progressionInt && progressionInt != 0) {
         currentEpisodeInt = episodeInt;
-        highlight_element(document.getElementById('box' + String(episodeInt)), clickedOpacity);
         create_episodePopUp(episodeInt);
     }
 }
@@ -83,7 +74,6 @@ function create_episodePopUp(episodeInt){
 
         //Getting references from box / index.html
         var popUp = document.getElementById('popUpBox');
-        var popUpBG = document.getElementById('popUpBackground');
         var popUpHeader = document.getElementById('popUpHeader');
         var popUpBody = document.getElementById('popUpBody');
         var popUpSearchBar = document.getElementById('popUpSearchBar');
@@ -95,11 +85,10 @@ function create_episodePopUp(episodeInt){
         empty_innerHTML();
 
         popUp.style.display = 'block';
-        popUpBG.style.backgroundColor = popUpBGColor;
         popUp.classList.toggle("openBox", true);
 
         //filling the box with content (one after one)
-        sleep(textDelay + animTimeFloat * 1000).then(() => {
+        sleep(animTimeFloat * 1000).then(() => {
             closeVar.style.display = 'block';
             popUpHeader.innerHTML = titles[episodeInt - 1];
             popUpHeader.appendChild(closeVar);
@@ -122,26 +111,62 @@ function check_Keyword(event) {
         var solution = document.getElementById('popUpSearchBar').value;
         for (var i = 0; i < keywords[currentEpisodeInt-1].length; i++)
             if (keywords[currentEpisodeInt-1][i] == solution) {
-                close_PopUp();
-                //progress, if the solution for the last unlocked episode is typed in (hardcoded: in case of episode 2, the two last unlocked episodes)
-                if (progressionInt == currentEpisodeInt || (progressionInt == 3 && currentEpisodeInt == 2)) {
-                    change_map();
-                    sleep(animTimeFloat*2 * 1000).then(() => {
-                        unlock_nextEpisode();
-                        if (progressionInt == 2) {
-                            //hardcoded, because two stations appear at the same time for episode 2
-                            unlock_nextEpisode();
-                        }
-                    })
-                }
+                change_map();
+                sleep(closePopUpAnimTime * 1000).then(() => {
+                    close_PopUp();
+
+                    //case: episode 1 unlocking 2 and 3
+                    if (progressionInt == 1 && currentEpisodeInt == 1) {
+                        sleep(closePopUpAnimTime * 1000).then(() => {
+                            show_stationIcon(progressionInt);
+                            sleep(closePopUpAnimTime * 1000).then(() => {
+                                progressionInt = 3;
+                                unlock_arrow(progressionInt - 1);
+                                unlock_arrow(progressionInt);
+                            })
+                        })
+                    }
+
+                    //case: episode 2 or 3 unlocking 4
+                    else if (progressionInt == 3 && (currentEpisodeInt == 2 || currentEpisodeInt == 3)) {
+                        sleep(closePopUpAnimTime * 1000).then(() => {
+                            show_stationIcon(progressionInt - 1);
+                            show_stationIcon(progressionInt);
+
+                            sleep(closePopUpAnimTime * 1000).then(() => {
+                                progressionInt = 4;
+                                unlock_arrow(progressionInt);
+                            })
+                        })
+                    }
+
+                    //general case: unlocking next station (unlocking next arrow showing last station icon)
+                    else if (progressionInt == currentEpisodeInt) {
+                        sleep(closePopUpAnimTime * 1000).then(() => {
+                            show_stationIcon(progressionInt);
+                            sleep(closePopUpAnimTime * 1000).then(() => {
+                                progressionInt++;
+                                unlock_arrow(progressionInt);
+                            })
+                        })
+                    }
+                })
             }
     }
 }
 
-function unlock_nextEpisode() {
-    progressionInt++;
-    highlight_station(progressionInt, unlockedOpacity);
-    highlight_element(document.getElementById('arrow' + String(progressionInt - 1)), clickedOpacity);
+
+function unlock_arrow(arrowInt) {
+    highlight_element(document.getElementById("arrow" + String(arrowInt)), unlockedOpacity);
+}
+
+function show_stationIcon(stationInt) {
+    highlight_element(document.getElementById("box" + String(stationInt)), unlockedOpacity);
+    fade_arrow(stationInt);
+}
+
+function fade_arrow(arrowInt) {
+    highlight_element(document.getElementById("arrow" + String(arrowInt)), clickedOpacity);
 }
 
 function change_map() {
@@ -151,7 +176,6 @@ function change_map() {
 function close_PopUp() {
     if (displayed) {
         displayed = false;
-        highlight_element(document.getElementById('box' + String(currentEpisodeInt)), unlockedOpacity);
         var popUp = document.getElementById('popUpBox');
         popUp.classList.toggle("openBox", false);
         popUp.classList.toggle("popUpBox", true);
@@ -186,6 +210,7 @@ function load_welcomeTxtFile() {
             content = txtFile.responseText;
         }
         document.getElementById("welcomeMessage").innerHTML = content;
+        document.getElementById("welcomeMessage").appendChild(proceedVar);
     }
     txtFile.send(null);
 }
